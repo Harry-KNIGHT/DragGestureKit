bc15e7d5839244df48668d938dd6d3a6356c8d8f
Elliot Knight <63256761+Harry-KNIGHT@users.noreply.github.com>
2024-02-15 21:45:40 +0100

    feat: refac (#6)
    * feat: add onended on drag gesture and add horizontal and vertical exemples
    
    * feat: mmove files on folders

---
+++ Sources/DragGestureKit/Exemples/HorizontalDraggableView.swift
@@ -0,0 +1,37 @@
+//
+//  HorizontalDraggableView.swift
+//
+//
+//  Created by Elliot Knight on 15/02/2024.
+//
+
+import SwiftUI
+
+struct HorizontalDraggableView: View {
+    @State var data: [String]
+    @State private var letterOnDrag: String?
+    var body: some View {
+        VStack {
+            DraggableView(data: $data, axis: .horizontal) {
+                HStack {
+                    ForEach(data, id: \.self) { letter in
+                        Text(letter)
+                            .padding(35)
+                            .font(letter == letterOnDrag ? .title2 : .title3)
+                            .background(letter == letterOnDrag ? Color.green : Color.secondary)
+                            .cornerRadius(10)
+                            .scaleEffect(letter == letterOnDrag ? 1.2 : 0.9)
+                            .shadow(radius: 10)
+                            .animation(.easeInOut, value: letterOnDrag)
+                    }
+                }
+            } onDragChanged: { value in
+                letterOnDrag = value
+            }
+        }
+    }
+}
+
+#Preview {
+    HorizontalDraggableView(data: ["1", "2", "3", "4"])
+}
